66613a7dec8140bae5b0f77132e0e0eac175ac7b
JHdez96 <156534102+JHdez96@users.noreply.github.com>
2024-01-13 12:02:16 -0500

    Add files via upload

---
+++ 2. Variables_compuestas.py
@@ -0,0 +1,9 @@
+lista = ["Yamid", "Dania", 23, True]
+print(lista)
+lista[3] = "Luz"
+print(lista)
+print(lista[0])
+print("Yamid" in lista)
+tupla = ("Yamid", "Dania", 23, True)
+print(tupla)
+print(type(tupla))
---
+++ 7. Empaquetado_desempaquetado.py
@@ -0,0 +1,23 @@
+
+#Empaquetado de datos
+cadena_en_tupla = ("Jeyson", "Yamid", 27)
+cadena_en_lista = ["Jeyson", "Yamid", 27]
+
+#Desempaquetado de datos
+nombre, apellido, edad = cadena_en_lista
+
+print(apellido)
+print(cadena_en_lista[1])
+
+
+
+
+
+
+
+Lista_personal = ("Jeyson", "Dania", "Keko")
+
+Esposo, Esposa, Suegro = Lista_personal
+
+print(Esposo)
+print(Esposa)
