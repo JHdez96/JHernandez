70bcf3e4f0c5c672a2198fea6f77e7920be53bc9
JHdez96 <156534102+JHdez96@users.noreply.github.com>
2024-01-13 12:38:44 -0500

    Add files via upload

---
+++ Errores_2.py
@@ -0,0 +1,28 @@
+def IMC(peso, altura):
+    try:
+        resultado = round(peso / altura**2, 2)
+        return resultado
+        if altura >= 0:
+           raise ZeroDivisionError ("No se puede dividir entre cero")
+    except:
+        resultado
+
+    
+
+def validacion(resultado):
+    if resultado >= 18.5 and resultado <= 24.9:
+        print("Persona saludable")
+    elif resultado >= 25:
+        print("Persona con sobrepeso")
+    elif resultado < 18.5:
+        print("Persona baja de peso")
+
+Peso = int(input("Ingrese su peso (kg): "))
+Altura = float(input("Ingrese su altura (m): "))
+
+resultado_IMC = IMC(Peso, Altura)
+print("Su IMC es:", resultado_IMC)
+(validacion(resultado_IMC))
+
+
+
